e82a91dfe5ee15d8a9d63f7ba672aa7cee4245c0
Jean Detoeuf <thebignet@gmail.com>
2016-06-07 08:56:04 +0200

    update to version 1.6.0 of swagger-codegen-gradle-plugin

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
-
 https://github.com/thebignet/swagger-codegen-gradle-plugin
-
-
-TODO
-
-* _rm -rf src/swagger; ./gradlew test_ must work
-* resolve all _TODOs_ in build.gradle
-* _./gradlew clean_ must clean generated code
--- src/test/java/io/swagger/petstore/client/jersey1/tests/PetstoreTest.java
+++ src/test/java/io/swagger/petstore/client/jersey1/tests/PetstoreTest.java
@@ -1,21 +1,21 @@
 package io.swagger.petstore.client.jersey1.tests;
-import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
-
-import io.swagger.petstore.client.jersey1.ApiClient;
-import io.swagger.petstore.client.jersey1.ApiException;
+import io.swagger.petstore.client.jersey1.api.NotFoundException;
 import io.swagger.petstore.client.jersey1.api.PetApi;
 import io.swagger.petstore.client.jersey1.model.Pet;
+import org.junit.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.Assert.assertEquals;
 
 public class PetstoreTest {
-	
-    @Test public void testSomeLibraryMethod() throws ApiException {
-		ApiClient apiClient = new ApiClient();
-		apiClient.setBasePath("http://petstore.swagger.io/v2");
-		PetApi petApi = new PetApi(apiClient);
-		Pet pet = petApi.getPetById(1L);
-		assertEquals("a", pet.getName());
-    }
+
+  @Test
+  public void testSomeLibraryMethod() throws NotFoundException {
+    PetApi petApi = new PetApi();
+    final ResponseEntity<Pet> pet = petApi.getPetById(1L);
+    assertEquals(HttpStatus.OK, pet.getStatusCode());
+  }
 
 }
