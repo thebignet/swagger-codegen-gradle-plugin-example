a201f6d6802ee375a23cecd348c7f0f6b0172e37
Jean Detoeuf <thebignet@gmail.com>
2017-08-03 08:11:17 +0200

    updated example to 1.7.4

--- src/test/java/io/swagger/petstore/client/jersey1/tests/PetstoreTest.java
+++ src/test/java/io/swagger/petstore/client/jersey1/tests/PetstoreTest.java
@@ -1,21 +1,19 @@
 package io.swagger.petstore.client.jersey1.tests;
 
-import io.swagger.petstore.client.jersey1.api.NotFoundException;
-import io.swagger.petstore.client.jersey1.api.PetApi;
-import io.swagger.petstore.client.jersey1.model.Pet;
+import io.swagger.petstore.client.ApiException;
+import io.swagger.petstore.client.api.PetApi;
+import io.swagger.petstore.client.model.Pet;
 import org.junit.Test;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
 import static org.junit.Assert.assertEquals;
 
 public class PetstoreTest {
 
   @Test
-  public void testSomeLibraryMethod() throws NotFoundException {
+  public void testSomeLibraryMethod() throws ApiException {
     PetApi petApi = new PetApi();
-    final ResponseEntity<Pet> pet = petApi.getPetById(1L);
-    assertEquals(HttpStatus.OK, pet.getStatusCode());
+    final Pet pet = petApi.getPetById(1L);
+    assertEquals(Pet.StatusEnum.AVAILABLE, pet.getStatus());
   }
 
 }
